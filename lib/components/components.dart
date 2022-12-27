a1d92cad0cc37c39d6e0ecdc54b12bee5d8f5ecb
Shaza Ashraf <Shazaashraf75@gmail.com>
2022-12-27 12:03:02 +0300

    editing duration

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -126,6 +126,7 @@ Widget accountWidget(
 Route animateToRoute(Widget page) {
   return PageRouteBuilder(
       pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionDuration: const Duration(milliseconds: 600),
       transitionsBuilder: (context, animation, secondaryAnimation, child) =>
           FadeTransition(
             opacity: animation,
